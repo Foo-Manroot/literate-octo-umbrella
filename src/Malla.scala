eb5c2422f692ff2a05fb9f1f9a0750d467df58e3
klosehacks <daniestrom@gmail.com>
2017-03-31 09:19:43 +0200

    Add files via upload

--- src/Malla.scala
+++ src/Malla.scala
@@ -1,10 +1,10 @@
-/**
+/*
  * Clase para representar la malla del juego.
  *
  * @param dim_filas
  *        Nº de filas que debe tener la matriz de juego.
  *
- * @param dim_cols
+ * @param dim_cols 
  *        Nº de columnas que debe tener la matriz de juego.
  */
 class Malla (dim_filas: Int, dim_cols: Int) {
@@ -13,7 +13,7 @@ class Malla (dim_filas: Int, dim_cols: Int) {
   val columnas: Int = dim_cols;
   val matriz: List [Int] = inicializar_matriz;
 
-  /**
+  /*
    *  Inicializa la lista con (filas * columnas) elementos aleatorios.
    *
    *  @return
@@ -24,7 +24,7 @@ class Malla (dim_filas: Int, dim_cols: Int) {
     List.fill (filas * columnas)(util.Random.nextInt (10))
   }
 
-  /**
+  /*
    *  Imprime el contenido de la matriz, ya formateado. Para imprimir el contenido de
    *  la matriz, sólo hace falta usar esta función tal que malla.imprimir_matriz()
    *
@@ -45,7 +45,7 @@ class Malla (dim_filas: Int, dim_cols: Int) {
     }
   }
 
-  /**
+  /*
    * Obtiene el elemento en la posición fila:columna.
    *
    * @param fila
