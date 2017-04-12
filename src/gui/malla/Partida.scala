55e1a3c4b2c4f88cdb7bfc4407b7980a1a512139
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-04-12 13:48:53 +0200

    Carpetas reorganizadas -> Carpeta cli/ para la implementación en línea de comandos -> Carpeta gui/ para la implementación con interfaz gráfica

---
+++ src/gui/malla/Partida.scala
@@ -0,0 +1,97 @@
+/* -------------------------------------------------------- */
+/* Estructura para almacenar los datos de la malla de juego */
+/* -------------------------------------------------------- */
+package malla
+
+import utils.Utils
+
+/**
+ * Clase para representar la malla del juego.
+ *
+ * @param dim_filas
+ *        Nº de filas que debe tener la matriz de juego.
+ *
+ * @param dim_cols
+ *        Nº de columnas que debe tener la matriz de juego.
+ *
+ * @param niv
+ *        Nivel del juego
+ */
+class Partida (dim_filas: Int, dim_cols: Int, niv: Int) {
+
+  val filas: Int = dim_filas
+  val columnas: Int = dim_cols
+  val nivel: Int = niv
+
+  /**
+   * Devuelve una cadena con la información de la partida, incluyendo el estado actual
+   * pasado como argumento.
+   *
+   * @param estado
+   *          Tupla con la puntuación y la matriz de juego (en ese orden)
+   */
+  def toString (estado: (Int, List [Any])): String = {
+
+    "Filas: " + filas + "\n"          +
+    "Columnas: " + columnas + "\n"    +
+    "Nivel: " + nivel + "\n"          +
+    "Puntuación: " + estado._1 + "\n" +
+    /* lista.toString devuelve "List(1, 2, 3, ...)"; así que se sustituyen los
+     * elementos innecesarios para devolver "1 2 3 4 5..." */
+    estado._2.toString.replace (',', ' ')
+                      .replace ('L', ' ')
+                      .replace ('i', ' ')
+                      .replace ('s', ' ')
+                      .replace ('t', ' ')
+                      .replace ('(', ' ')
+                      .replace (')', ' ')
+                      .trim
+  }
+
+  /**
+   *  Imprime el contenido de la matriz, ya formateado. Para imprimir el contenido de
+   *  la matriz, sólo hace falta usar esta función tal que malla.imprimir_matriz()
+   *
+   *  @param lista
+   *          Lista a ser impresa, con el número de de columnas especificado en la
+   *        declaración del objeto. Puede omitirse (por defecto, es 'matriz').
+   *
+   *  @param cols
+   *          Número de columnas para formatear la matriz. Puede omitirse (por defecto,
+   *        es el valor de "columnas")
+   */
+  def imprimir_matriz (lista: List [Any], cols: Int = columnas): Unit = {
+
+    Utils.mapear_indexado (lista) {
+      /* Si se trata de una lista de enteros, imprime los colores. Si no, imprime
+      directamente el contenido */
+      case (e: Int , i) =>  if (i % cols == (cols - 1)) {
+
+                              print (diamante (e) + "\n")
+                            } else {
+
+                              print (diamante (e) + " ")
+                            }
+
+      case (e, i) => if (i % cols == (cols - 1)) print (e + "\n") else print (e + " ")
+    }
+  }
+
+  /**
+   * Dvuelve una cadena para representar el diamante con color, según su valor numérico.
+   */
+  def diamante (valor: Int): String = {
+
+    valor match {
+      case 1 => Console.BLUE                  + "1" + Console.RESET
+      case 2 => Console.RED                   + "2" + Console.RESET
+      case 3 => Console.YELLOW                + "3" + Console.RESET
+      case 4 => Console.GREEN                 + "4" + Console.RESET
+      case 5 => Console.CYAN                  + "5" + Console.RESET
+      case 6 => Console.MAGENTA               + "6" + Console.RESET
+      case 7 => Console.BLUE + Console.BOLD   + "7" + Console.RESET
+      case 8 => Console.WHITE                 + "8" + Console.RESET
+      case _ => Console.WHITE + Console.BOLD  + "#" + Console.RESET
+    }
+  }
+}
---
+++ src/gui/utils/Factoría.scala
@@ -0,0 +1,35 @@
+/* ----------------------------------- */
+/* Definiciones de métodos de factoría */
+/* ----------------------------------- */
+package utils
+
+import malla.Partida
+
+
+object Factoría {
+
+  /**
+   * Método de factoría para crear un objeto de tipo Partida en función del nivel
+   * especificado.
+   *
+   * @param niv
+   *            Nivel con el que crear el tablero
+   *
+   * @return
+   *            Una nueva instancia de una partida, si se trata de un nivel aceptado.
+   *          Si no, lanza una excepción.
+   *
+   * @throws
+   *            Error -> si el nivel especificado no está reconocido.
+   */
+  def crear_partida (niv: Int): Partida = {
+
+    niv match {
+
+      case 1 => new Partida ( 7,  9, niv)
+      case 2 => new Partida (11, 17, niv)
+      case 3 => new Partida (15, 27, niv)
+      case _ => throw new Error ("El nivel '" + niv + "' no está soportado")
+    }
+  }
+}
